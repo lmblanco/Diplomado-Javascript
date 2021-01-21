134c6be5897ad5119bb7cc8466e67c3612b95e57
Miguel Blanco <lmiguelblancoc@gmail.com>
2021-01-20 19:55:27 -0500

    primeros pasos

---
+++ primeros_Pasos_JS_20_01_2021/Clase 4-20-01-2021/main.js
@@ -0,0 +1,36 @@
+console.log("Miguel Blanco");
+
+// Tipos de datos 
+
+// String cadena
+"Miguel"
+"Blanco Carrillo"
+
+// Numeros
+1000
+2.5
+-25
+
+//Booleanos
+true
+false
+
+
+//array listas
+['Miguel','Blanco','Carrillo']
+
+[1,2,3,4]
+
+[true, false]
+
+
+// Objetos 
+{
+   nombre:'Miguel',
+   apellido:'Blanco',
+   edad: 25,
+   profesor: false,
+   antiguedad: false
+}
+
+
