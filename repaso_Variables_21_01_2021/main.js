3683ee55f5bddb81c524437ebd7130da76e14900
Miguel Blanco <lmiguelblancoc@gmail.com>
2021-01-25 12:57:14 -0500

    cambios

---
+++ repaso_Variables_21_01_2021/main.js
@@ -0,0 +1,41 @@
+// Tipos de datos
+// string
+// "Miguel"
+// "Blanco"
+
+// Number
+// 123456789
+// 1.22
+// 1,55
+// -4455
+// -1.55
+// -1.548
+
+// Boolen
+// false
+// true
+
+// Array
+// ["Miguel", 25, true]
+
+// Object
+// { name: "Miguel", edad: 25, soy_profesor: false }
+
+informacion = {id:1032};
+
+console.log(informacion);
+console.log(typeof(informacion));
+
+informacion = 23;
+console.log(typeof(informacion));
+
+informacion ="Miguel";
+
+console.log(informacion);
+console.log(typeof(informacion));
+
+function saludar(){
+    console.log("Hola");
+}
+
+saludar();
---
+++ variables_Constantes_20_01_2021/main.js
@@ -0,0 +1,36 @@
+console.log("Conectado");
+
+vaso = "rojo";
+console.log(vaso);
+
+vaso = "amarillo";
+console.log(vaso)
+
+nombre = "Miguel";
+edad = 25;
+estudiante = true;
+
+lista_cursos = ['html','JavaScript','PHP','C++','C#']
+
+informacion_personal = {
+    id: 1098779341,
+    direccion: "calle 105 # 22 - 25",
+    cursos: ['PHP','Javascript']
+}
+
+console.log(nombre);
+console.log(edad);
+console.log(estudiante);
+console.log(lista_cursos);
+console.log(informacion_personal);
+
+var nombre2 = "Miguel";
+let edad2 = 25;
+
+
+// cuando se declara la constante hay que inicializarla con
+// un valor 
+const cedula = 1098779341;
+
+// No se puede incluir un nuevo valor
+//cedula = 100;
