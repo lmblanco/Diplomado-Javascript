a27a52201f85b4626b7acfdeef5b90f3f5695aa2
Miguel Blanco <lmiguelblancoc@gmail.com>
2021-01-28 19:21:42 -0500

    Taller 2

---
+++ Taller2/condicionales_Edad_If_Else_FechaActual/main.js
@@ -0,0 +1,7 @@
+let edad = parseInt(prompt("Ingrese su edad: "));
+
+if (edad == 18 || edad > 18) {
+    alert("Es mayor de edad.");
+}else{
+    alert("La edad ingresada no cumple con los parametros");
+}
---
+++ Taller2/condicionales_Edad_Switch_Case_FechaActual/main.js
@@ -0,0 +1,11 @@
+let edad = parseInt(prompt("Ingrese su edad: "));
+
+switch (edad) {   
+    case 18:
+        alert("Es mayor de edad.");
+        break;
+
+    default:
+        alert("La edad ingresada no cumple con los parametros");
+        break;
+}
