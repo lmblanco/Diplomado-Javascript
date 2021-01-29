0982765943bc518fcf2cd4bf44107ac288cba171
Miguel Blanco <lmiguelblancoc@gmail.com>
2021-01-28 20:46:46 -0500

    act

--- for_for-in_for-of_28_01_2021/main.js
+++ for_for-in_for-of_28_01_2021/main.js
@@ -1,6 +1,6 @@
 //console.time("for()");
 
-let lista = ["miguel","adrian","jhon","diego"];
+//let lista = ["miguel","adrian","jhon","diego"];
 // for (let i = 0; i <= lista.length; i++) {
 //     console.log(lista[i]);   
 // }
@@ -17,8 +17,16 @@ let lista = ["miguel","adrian","jhon","diego"];
 // }
 
 // Otra forma de mostrarlo.
-for (let i = 0; i <= lista.length; i++) {
-    console.log(`la posicion de la lista es ${i} y su valor es ${lista[i]}`);   
-}
+// for (let i = 0; i <= lista.length; i++) {
+//     console.log(`la posicion de la lista es ${i} y su valor es ${lista[i]}`);   
+// }
 
+let lista = ["miguel", "andrea", "diana", "adrian", "diego", "julian"];
+let edad = [23,16,37,50,37,27];
+let info = [];
+
+for (let i = 0; i < lista.length; i++) {
+    info[i] = {name : lista[i], edad : edad[i]};
+}
+console.log(info);
 
